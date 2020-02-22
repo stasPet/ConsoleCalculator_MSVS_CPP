0cd0b226c10c57a440863ffb25a42752c2913886
Stanislav <petrov_stas@outlook.com>
2020-02-22 13:45:09 +0300

    Comments are added in file: StateOperation.cpp

--- ConsoleCalculator/Lexer/StateMachine/StateOperation.cpp
+++ ConsoleCalculator/Lexer/StateMachine/StateOperation.cpp
@@ -3,6 +3,11 @@
 using namespace clc::lxr;
 using namespace clc;
 
+/*
+     онструктор принимает строковые значени€, что бы использовать их
+    дл€ сравнени€ с вход€щими символами.
+*/
+
 StateOperation::StateOperation(LexemeType t,
     std::initializer_list<String> l)
 {
@@ -19,25 +24,31 @@ StateOperation::StateOperation(LexemeType t,
 
 LexemeType StateOperation::Set(Char message) 
 {
+    /*
+        ќбъект имеет 4 состо€ни€.
+    */
     switch (currentState)
     {
     case State::Check:
 
+        // ѕеребирам по очереди каждую стоку вектора fNamesRef
         for (auto p = fNamesRef.begin(); p != fNamesRef.end(); )
         {
             String const & element = *(*p);
 
+            // ¬ начале position = 0
             if (element[position] != message)
-                p = fNamesRef.erase(p);
+                p = fNamesRef.erase(p); // ≈сли вход€щий символ отсутствует в указанной позиции строки, удал€ем строку из вектора
             else
                 ++p;
         }
 
-        if (fNamesRef.size() == 1)
+        if (fNamesRef.size() == 1) // ≈сли в векторе осталась одна строка
         {
-            if (position + 1 == (*(fNamesRef.front() ) ).size() )
-                currentState = State::SetFlag;
-        }
+            if (position + 1 == (*(fNamesRef.front() ) ).size() ) // “о провер€ем position, если он равен размеру оставшейс€ в векторе строке, значит строка совпала
+                currentState = State::SetFlag; // ѕереводим в состо€ние  SetFlag, это нужно что бы пропустить один шаг, 
+        }                                      // потому что другие состо€ни€ не могут определить последовательность как коректною,
+                                               // если не прочитают следующий за коректной последовательностью символ
         else if (fNamesRef.empty() )
         {
             currentState = State::Fail;
