3396a8e75baffdc9b478f1513935a8209ecfa834
Stanislav <petrov_stas@outlook.com>
2020-02-22 13:49:42 +0300

    Comments are added in file: StateOperation.cpp

--- ConsoleCalculator/Lexer/StateMachine/StateOperation.cpp
+++ ConsoleCalculator/Lexer/StateMachine/StateOperation.cpp
@@ -4,8 +4,7 @@ using namespace clc::lxr;
 using namespace clc;
 
 /*
-    Конструктор принимает строковые значения, что бы использовать их
-    для сравнения с входящими символами.
+    The constructor accepts string values so that they can be used to compare with incoming characters.
 */
 
 StateOperation::StateOperation(LexemeType t,
@@ -25,30 +24,30 @@ StateOperation::StateOperation(LexemeType t,
 LexemeType StateOperation::Set(Char message) 
 {
     /*
-        Объект имеет 4 состояния.
+        The object has 4 states.
     */
     switch (currentState)
     {
     case State::Check:
 
-        // Перебирам по очереди каждую стоку вектора fNamesRef
+        // Iterate through each line of the vector fNamesRef in turn
         for (auto p = fNamesRef.begin(); p != fNamesRef.end(); )
         {
             String const & element = *(*p);
 
-            // В начале position = 0
+            // At the beginning position = 0
             if (element[position] != message)
-                p = fNamesRef.erase(p); // Если входящий символ отсутствует в указанной позиции строки, удаляем строку из вектора
+                p = fNamesRef.erase(p); // If the input character is not in the specified position of the string, remove the string from the vector
             else
                 ++p;
         }
 
         if (fNamesRef.size() == 1) // Если в векторе осталась одна строка
         {
-            if (position + 1 == (*(fNamesRef.front() ) ).size() ) // То проверяем position, если он равен размеру оставшейся в векторе строке, значит строка совпала
-                currentState = State::SetFlag; // Переводим в состояние  SetFlag, это нужно что бы пропустить один шаг, 
-        }                                      // потому что другие состояния не могут определить последовательность как коректною,
-                                               // если не прочитают следующий за коректной последовательностью символ
+            if (position + 1 == (*(fNamesRef.front() ) ).size() ) // That we check the position, if it is equal to the size of the row remaining in the vector, then the row coincided
+                currentState = State::SetFlag; // We set it to SetFlag state, this is necessary to skip one step,
+        }                                      // because other states cannot determine the sequence as correct,
+                                               // if they don’t read the character following the correct sequence
         else if (fNamesRef.empty() )
         {
             currentState = State::Fail;
